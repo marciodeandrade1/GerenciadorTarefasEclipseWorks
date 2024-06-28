5abece4844aee30eee326a98e5a6b1b48b79da71
Marcio de Andrade <marcio@requestdev.net>
2024-06-28 19:27:18 -0400

    Criação de Comandos para criar projetos e tarefas

--- GerenciadorTarefasEclipseWorks.Application/Commands/CriarProjetoCommand.cs
+++ GerenciadorTarefasEclipseWorks.Application/Commands/CriarProjetoCommand.cs
@@ -1,17 +1,6 @@
 ﻿using MediatR;
-using GerenciadorTarefasEclipseWorks.Domain.Entities;
 
 namespace GerenciadorTarefasEclipseWorks.Application.Commands
 {
-    public class CriarProjetoCommand : IRequest<int>
-    {
-        public string Nome { get; set; }
-    }
-
-    public class CriarTarefaCommand : IRequest<int>
-    {
-        public string Titulo { get; set; }
-        public string Descricao {  get; set; }
-        public string Prioridade {  get; set; }
-    }
+    public record CriarProjetoCommand(string Nome) : IRequest<int>;
 }
---
+++ GerenciadorTarefasEclipseWorks.Application/Commands/CriarTarefaCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace GerenciadorTarefasEclipseWorks.Application.Commands
+{
+    public record CriarTarefaCommand(string Titulo, string Descricao, string Prioridade, int ProjetcId) : IRequest<int>;
+}

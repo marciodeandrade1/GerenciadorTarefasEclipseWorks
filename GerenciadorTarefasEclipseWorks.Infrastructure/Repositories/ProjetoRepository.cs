3825ba9e0b731b89913ee9b4fa3c54b7e93240e6
Marcio de Andrade <marcio@requestdev.net>
2024-06-28 17:37:15 -0400

    Implementação dos repositórios com bases nas interfaces do Domain

---
+++ GerenciadorTarefasEclipseWorks.Infrastructure/Repositories/ProjetoRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GerenciadorTarefasEclipseWorks.Domain.Entities;
+using GerenciadorTarefasEclipseWorks.Domain.Interfaces;
+
+namespace GerenciadorTarefasEclipseWorks.Infrastructure.Repositories
+{
+    public class ProjetoRepository : IProjetoRepository
+    {
+        private readonly GerenciadorTarefasDbContext _context;
+        public ProjetoRepository(GerenciadorTarefasDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddProjetoAsync(Projeto projeto)
+        {
+            await _context.Projetos.AddAsync(projeto);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Projeto>> GetAllProjetosAsync()
+        {
+            return await _context.Projetos.ToListAsync();
+        }
+
+        public async Task<Projeto> GetProjetoByIdAsync(int id)
+        {
+#pragma warning disable CS8603 // Possible null reference return.
+            return await _context.Projetos
+                                 .Include(p => p.Tarefas)
+                                 .FirstOrDefaultAsync(p => p.Id == id);
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+
+        public async Task RemoveProjetoAsync(int id)
+        {
+            var projeto = await _context.Projetos.FindAsync(id);
+            if (projeto != null)
+            {
+                _context.Projetos.Remove(projeto);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ GerenciadorTarefasEclipseWorks.Infrastructure/Repositories/TarefaRepository.cs
@@ -0,0 +1,55 @@
+﻿using GerenciadorTarefasEclipseWorks.Domain.Entities;
+using GerenciadorTarefasEclipseWorks.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenciadorTarefasEclipseWorks.Infrastructure.Repositories
+{
+    public class TarefaRepository : ITarefaRepository
+    {
+        private readonly GerenciadorTarefasDbContext _context;
+
+        public TarefaRepository(GerenciadorTarefasDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddTarefaAsync(Tarefa tarefa)
+        {
+            await _context.Tarefas.AddAsync(tarefa);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Tarefa> GetTarefaByIdAsync(int id)
+        {
+#pragma warning disable CS8603 // Possible null reference return.
+            return await _context.Tarefas
+                .Include(t => t.Comentario)
+                .Include(t => t.AtualizaHistoricos)
+                .FirstOrDefaultAsync(t => t.Id == id);
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+
+        public async Task<List<Tarefa>> GetTarefasByProjetoIdAsync(int projetoId)
+        {
+            return await _context.Tarefas
+                .Where(t => t.ProjetoId == projetoId)
+                .ToListAsync();
+        }
+
+        public async Task RemoveTarefaAsync(int id)
+        {
+            var tarefa = await _context.Tarefas.FindAsync(id);
+            if (tarefa != null)
+            {
+                _context.Tarefas.Remove(tarefa);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task UpdateTarefaAsync(Tarefa tarefa)
+        {
+            _context.Tarefas.Update(tarefa);
+            await _context.SaveChangesAsync();
+        }
+    }
+}

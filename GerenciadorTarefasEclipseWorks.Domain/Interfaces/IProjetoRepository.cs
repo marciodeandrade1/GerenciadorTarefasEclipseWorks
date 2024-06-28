692e03b5d31a9db156086f2484e2d615474da91d
Marcio de Andrade <marcio@requestdev.net>
2024-06-28 16:13:56 -0400

    refatoração remoção de using desnecessários

--- GerenciadorTarefasEclipseWorks.Domain/Interfaces/IProjetoRepository.cs
+++ GerenciadorTarefasEclipseWorks.Domain/Interfaces/IProjetoRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GerenciadorTarefasEclipseWorks.Domain.Entities;
+﻿using GerenciadorTarefasEclipseWorks.Domain.Entities;
 
 namespace GerenciadorTarefasEclipseWorks.Domain.Interfaces
 {

87e8c40fe5586c2f105072c1ff0d0233e6fb05ad
Marcio de Andrade <marcio@requestdev.net>
2024-06-28 21:42:59 -0400

    Programação dos controladores para Projetos e Tarefas que utilizem MediatR para manipular comandos e consultas.

--- GerenciadorTarefasEclipseWorks.API/Program.cs
+++ GerenciadorTarefasEclipseWorks.API/Program.cs
@@ -1,9 +1,21 @@
+using GerenciadorTarefasEclipseWorks.Domain.Interfaces;
+using GerenciadorTarefasEclipseWorks.Infrastructure.Data;
+using GerenciadorTarefasEclipseWorks.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<GerenciadorTarefasDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IProjetoRepository, ProjetoRepository>();
+builder.Services.AddScoped<ITarefaRepository, TarefaRepository>();
+
+//builder.Services.AddMediatR(typeof(Program).Assembly);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 

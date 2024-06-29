0601e942ff4420686743a60b0196cfae329b06bd
Marcio de Andrade <marcio@requestdev.net>
2024-06-28 21:27:34 -0400

    Criação Controllers Projetos, Tarefas

---
+++ GerenciadorTarefasEclipseWorks.API/Controllers/ProjetosController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using GerenciadorTarefasEclipseWorks.Application.Commands;
+using GerenciadorTarefasEclipseWorks.Application.Queries;
+
+
+namespace GerenciadorTarefasEclipseWorks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjetosController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProjetosController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProjetos()
+        {
+            var projetos = await _mediator.Send(new GetAllProjetosQuery());
+            return Ok(projetos);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarProjeto([FromBody] CriarProjetoCommand command)
+        {
+            var projetoId = await _mediator.Send(command);
+            return Ok(projetoId);
+        }
+    }
+}
---
+++ GerenciadorTarefasEclipseWorks.API/Controllers/TarefasController.cs
@@ -0,0 +1,33 @@
+﻿using GerenciadorTarefasEclipseWorks.Application.Commands;
+using GerenciadorTarefasEclipseWorks.Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciadorTarefasEclipseWorks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefasController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TarefasController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{projetoId}")]
+        public async Task<IActionResult> GetTarefaByProjetoId(int projetoId)
+        {
+            var tarefas = await _mediator.Send(new GetTarefasByProjetoIdQuery(projetoId));
+            return Ok(tarefas);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarTarefa([FromBody] CriarTarefaCommand command)
+        {
+            var tarefaId = await _mediator.Send(command);
+            return Ok(tarefaId);
+        }
+    }
+}
